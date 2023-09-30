b0ee9d7ed2bac00e2f0b361b320f311d2d82563f
llam36 <llam36@gatech.edu>
2023-09-30 00:11:30 -0400

    fix: stop timerAC when reach delay 2 timer

--- src/main.c
+++ src/main.c
@@ -1028,14 +1028,13 @@ void interrupt_timer0()
                val_timer_ktra_AC--;
             break;
          case 1: // TIMER CO AC
-            if (++flag_timer_AC_60s > 59)
+            if (val_timer_AC < timer_chay_lien_tuc && ++flag_timer_AC_60s > 59)
             {
                flag_timer_AC_60s = 0;
                if (++flag_timer_AC_60p > 59)
                {
                   flag_timer_AC_60p = 0;
-                  if (val_timer_AC < timer_chay_lien_tuc)
-                     val_timer_AC++;
+                  val_timer_AC++;
                }
             }
             break;
