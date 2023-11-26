5bc06f48b75afaba9f7640018cc4c97a415600c3
Long Lam <lamhoanglong.3001@gmail.com>
2023-11-26 10:19:29 -0500

    fix: try light test

--- src/main.c
+++ src/main.c
@@ -338,6 +338,7 @@ void verify_dc(void)
    }
    else if (adc_accu <= input_dc_lv2 - delta_dc)
    {
+      output_high(out_error);
       delay_ms(200);
       get_adc_accu();
       if (adc_accu <= DC_LOW_LVL_2)
