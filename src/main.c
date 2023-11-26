1a5d22f333625ba2d8d30fd204659ccec4c862dc
Long Lam <lamhoanglong.3001@gmail.com>
2023-11-26 11:10:47 -0500

    fix: debug 6

--- src/main.c
+++ src/main.c
@@ -353,6 +353,8 @@ void verify_dc(void)
    else
    {
       output_high(out_error);
+      delay_ms(2000);
+      output_low(out_error);
    }
 }
 
@@ -461,7 +463,7 @@ void display(char code_print)
       PRINTF(LCD_PUTCHAR, "KIEM TRA AC");
       clear_lcd();
       LCD_PUTCMD(Line_2);
-      PRINTF(LCD_PUTCHAR, "00:%02u:%02u %02.1fV", sec_to_minute(val_timer_ktra_AC), sec_to_sec(val_timer_ktra_AC), temp);
+      PRINTF(LCD_PUTCHAR, "00:%02u:%02u %02.1fV", sec_to_minute(val_timer_ktra_AC), sec_to_sec(val_timer_ktra_AC), adc_accu);
       clear_lcd();
       break;
    case 5: // AC BINH THUONG
